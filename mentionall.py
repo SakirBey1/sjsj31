22cdced47b79b6557e65a47cd46a0701dde01123
SakirBey1 <sakirhack81@gmail.com>
2022-09-04 02:58:27 +0300

    Create mentionall.py

---
+++ mentionall.py
@@ -0,0 +1,47 @@
+import random, os, logging, asyncio
+from telethon import Button
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+from telethon.tl.types import ChannelParticipantsAdmins
+from asyncio import sleep
+from Config import Config
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(name)s - [%(levelname)s] - %(message)s'
+)
+LOGGER = logging.getLogger(__name__)
+
+api_id = Config.API_ID
+api_hash = Config.API_HASH
+bot_token = Config.BOT_TOKEN
+bot_username = Config.BOT_USERNAME
+support = Config.SUPPORT_CHAT
+owner = Config.OWNER_USERNAME
+bot_name = Config.BOT_NAME
+
+
+SUDO_USERS = Config.SUDO_USERS
+
+client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
+
+anlik_calisan = []
+
+tekli_calisan = []
+
+ozel_list = [1948748468]
+anlik_calisan = []
+grup_sayi = []
+etiketuye = []
+rxyzdev_tagTot = {}
+rxyzdev_initT = {}
+
+
+
+
+
+
+@client.on(events.NewMessage(pattern='merhaba'))
+async def handler(event):
+	
+    await event.reply('Kes aptal kürt')
